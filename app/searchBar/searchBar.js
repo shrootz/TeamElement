6c0acd61742889b2f79322774ffc749b54c89a52
Jeremy Castillo <jeremy.castillo14@utexas.edu>
2016-07-07 11:17:21 -0400

    hopefully most stable version

--- app/searchBar/searchBar.js
+++ app/searchBar/searchBar.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function SearchBarController($scope,$http) {
+function SearchBarController($scope, $http) {
 	var customerAmtArray = [];
+	$scope.finalArr = [[]];
   	$scope.onclick = function(){
 	  	$http({
 		  method: 'GET',
@@ -15,18 +16,41 @@ function SearchBarController($scope,$http) {
 				  method: 'GET',
 				  url: url
 				}).then(function successCallback(response) {
-					var id = 0;
+					var total_spent = 0;
 					var food = 0;
 					var clothes = 0;
 					var other = 0;
 					var merchant_id = 0;
 					for (var i = 0; i<response.data.length; i++){
-						id = id +response.data[i].amount;
-						merchant_id = response.data[i].merchant_id;
+						total_spent = total_spent +response.data[i].amount;
+						merchant_id = response.data[i].payee_id;
 					}
-					customerAmtArray.add(id);
-					console.log(id);
-					console.log(response.data)
+					customerAmtArray.push(total_spent);
+
+					var sortedArr = _.sortBy(customerAmtArray, function(num) { return parseInt(num); });
+
+					var groupedArr = _.groupBy(sortedArr, function(num) {
+						var group = 0;
+						var amt = parseInt(num);
+						if(amt > 0 && amt < 5000) { group = 1; }
+						if(amt >= 5000 && amt < 10000) { group = 2; }
+						else if(amt >= 10000 && amt <= 15000) { group = 3; }
+						else if(amt > 15000 && amt <= 20000) {group = 4;}
+						else if(amt > 20000 && amt <= 25000) {group = 5;}
+						else if(amt > 25000 && amt <= 30000) {group = 6;}
+						else if(amt > 30000 && amt <= 35000) {group = 7;}
+						else if(amt > 35000) {group = 8;}
+						return group;
+					});
+
+					var finalArr = _.map(groupedArr, function(arr) {
+						return arr.length;
+					});
+
+					$scope.finalArr = [finalArr];
+
+					console.log($scope.finalArr);
+
 			  	}, function errorCallback(response) {
 
 			  	});
@@ -35,6 +59,10 @@ function SearchBarController($scope,$http) {
 
 	  	});
 	};
+
+	$scope.go = function() {
+		console.log("hello");
+	}
 }
 
 angular.module('myApp').component('search', {
